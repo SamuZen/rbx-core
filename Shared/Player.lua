58e4918e5c15fd721e009a3468a550434a0db150
Samu <Pedroqueiroz.zen@gmail.com>
2024-11-16 13:45:02 -0300

    remove unecessary modules for core

--- Shared/Player.lua
+++ Shared/Player.lua
@@ -1,22 +1,6 @@
-local Character = require(script.Parent.Character.Utils)
-
 local Players = game:GetService("Players")
 local Player = {}
 Player.__index = Player
-
-local localPlayer = Players.LocalPlayer
-
-function Player.getHumanoidRootPart(player: Player): BasePart
-    if player and player.Character then
-        local part = player.Character:FindFirstChild("HumanoidRootPart")
-        return part
-    end
-end
-
-function Player.isLocalPlayerPart(part: BasePart): boolean
-    if localPlayer.Character == nil then return false end
-    return part:IsDescendantOf(localPlayer.Character)
-end
 
 function Player.onPlayerAdded(callback: (player: Player) -> nil): RBXScriptConnection
     local connection
@@ -36,12 +20,6 @@ function Player.onPlayerRemoving(callback: (player: Player) -> nil): RBXScriptCo
         callback(player)
     end)
     return connection
-end
-
-function Player.getPlayerFromPart(part: BasePart): Player | nil
-    local character = Character.getCharacterFromPart(part)
-    if character == nil then return nil end
-    return Players:GetPlayerFromCharacter(character)
 end
 
 return Player
--- Shared/Weld.lua
+++
@@ -1,9 +0,0 @@
-local function _weld(p0: BasePart, p1: BasePart): WeldConstraint
-    local weld = Instance.new("WeldConstraint")
-    weld.Parent = p0
-    weld.Part0 = p0
-    weld.Part1 = p1
-    return weld
-end
-
-return _weld
